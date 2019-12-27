274f5b6dc1f113fe86b3baf48579d7910f141c69
Naoki Matsumoto <m.naoki9911@gmail.com>
2019-12-27 12:48:09 +0900

    Add TestUnit

---
+++ src/main/scala/TestUnit.scala
@@ -0,0 +1,16 @@
+import chisel3.Module
+import chisel3.{util, _}
+
+class TestUnit(implicit val conf: CAHPConfig) extends Module{
+  val io = IO(new IfUnitOut)
+
+  val ifUnit = Module(new IfUnit())
+  val rom = Module(new ExternalTestRom)
+
+  ifUnit.io.in.romData := rom.io.romData
+
+  ifUnit.io.in.jumpAddress := 0.U
+  ifUnit.io.in.jump := false.B
+
+  rom.io.romAddress := ifUnit.io.out.romAddress
+}
