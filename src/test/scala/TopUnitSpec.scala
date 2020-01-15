c2f797d6de4627583dacb0c85e1a57714d44da22
Naoki Matsumoto <m.naoki9911@gmail.com>
2020-01-15 10:06:53 +0900

    Add Test 0009-gameoflife

--- src/test/scala/TopUnitSpec.scala
+++ src/test/scala/TopUnitSpec.scala
@@ -33,7 +33,7 @@ class TopUnitSpec() extends ChiselFlatSpec {
 
   val testDir = new File("src/test/binary/")
   testDir.listFiles().foreach { f =>
-    if (f.getName().contains("0008-rpn.bin")) {
+    if (f.getName().contains("0009-gameoflife.bin")) {
       println(f.getName())
       val parser = new TestBinParser(f.getAbsolutePath())
       conf.testRom = parser.romSeq
